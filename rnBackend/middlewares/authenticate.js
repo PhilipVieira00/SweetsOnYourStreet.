d988a536dc7994501817aeeed0022c948e4683e0
Clara Ayala Garcia <claraayala@ymail.com>
2019-10-19 11:28:29 -0400

    Create a React app and publish it to GitHub Pages

---
+++ rnBackend/middlewares/authenticate.js
@@ -0,0 +1,18 @@
+const {User} = require("../models/user");
+
+const authenticate = (req, res, next) => {
+    var token = req.header("x-auth");
+    User.findUserByToken(token).then((user) => {
+        if(!user) {
+            Promise.reject();
+        } else {
+            req.user = user;
+            req.token = token;
+            next();
+        }
+    }).catch((error) => {
+        res.status(401).send();
+    });
+}
+
+module.exports = authenticate;
---
+++ src/components/Buttons.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/Buttons.css";
+
+
+
+
+function Buttons() {
+    return (
+      <div>
+        
+          <button id="button" type="button" className="btn btn-lg btn-light" href="/registration">
+                Driver Registration</button>
+                <br></br>
+                <br></br>
+  
+          <button id="button" type="button" className="btn btn-lg btn-light" href="/registration">
+                Driver Login</button>
+
+           
+      </div>  
+
+
+        
+        
+     
+     
+    );
+  }
+  
+  export default Buttons;
